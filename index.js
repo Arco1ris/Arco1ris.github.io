0f9c783f919ab3ef02d81fbe83cb3cae09741822
rainbow <49795641@qq.com>
2015-02-11 14:46:47 +0800

    change

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $( "#menu_1" ).click(function() {
+        $("#p1").load("index.html");
+        $('html, body').animate({
+            scrollTop: $("#page_1").offset().top
+        }, 700);
+    });
+    
+    /*$( "#menu_2" ).click(function() {
+        $("#p1").load("portfolio.html");
+        $('html, body').animate({
+            scrollTop: $("#page_1").offset().top
+        }, 700);
+    });
+    
+    $('#poplemon').popup();
+    $('#poplemon').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 700);
+    });*/
+    
+});
